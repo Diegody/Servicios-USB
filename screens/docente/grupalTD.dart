f8f572ece87c43eb40d394cfecd4a2cfa2c3f6dc
Diegody <diegogody12@gmail.com>
2024-02-21 17:55:17 -0500

    Tabla de grupos

--- screens/docente/grupalTD.dart
+++ screens/docente/grupalTD.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:servicios/globals.dart';
 
 class GrupalTutoriaScreen extends StatefulWidget {
   @override
@@ -10,6 +11,8 @@ class GrupalTutoriaScreen extends StatefulWidget {
 class _GrupalTutoriaScreenState extends State<GrupalTutoriaScreen> {
   // ignore: unused_field
   List<Map<String, dynamic>> _data = [];
+  List<Map<String, dynamic>> _filteredData = [];
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -20,13 +23,38 @@ class _GrupalTutoriaScreenState extends State<GrupalTutoriaScreen> {
   Future<void> fetchData() async {
     final response = await http.post(
       Uri.parse(
-          'https://academia.usbbog.edu.co/centralizacion_servicios_ios/API/Tutorias/DocentesTutoria/ListaEstudiantes.php'),
+          'https://academia.usbbog.edu.co/centralizacion_servicios_ios/API/Tutorias/DocentesTutoria/ListaGrupos.php'),
+      body: {
+        'DOC_DOC': globalCodigoDocente,
+      },
     );
     if (response.statusCode == 200) {
       setState(() {
         _data = List<Map<String, dynamic>>.from(json.decode(response.body));
+        _filteredData = _data;
       });
     } else {}
+  }
+
+  void _filterData(String query) {
+    setState(() {
+      _filteredData = _data
+          .where((item) =>
+              item.values.any((value) => value.toString().contains(query)))
+          .toList();
+    });
+  }
+
+  void _navigateToDetailsPage(String ciclo, String documento) {
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //     builder: (context) => SesionTDScreenD(
+    //       ciclo: ciclo,
+    //       documento: documento,
+    //     ),
+    //   ),
+    // );
   }
 
   @override
@@ -44,6 +72,127 @@ class _GrupalTutoriaScreenState extends State<GrupalTutoriaScreen> {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: _filterData,
+              decoration: InputDecoration(
+                labelText: 'Buscar',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          _filteredData.isEmpty
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No hay grupos creados actualmente',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Acción para crear grupo de tutoría
+                      },
+                      child: Text(
+                        'Crear grupo de tutoría',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                      ),
+                    ),
+                  ],
+                )
+              : Expanded(
+                  child: ListView(
+                    children: [
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                          dataTableTheme: DataTableThemeData(
+                            headingRowColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.orange,
+                            ),
+                            dataRowColor: MaterialStateColor.resolveWith(
+                              (states) =>
+                                  const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                        child: PaginatedDataTable(
+                          header: Text(
+                            'Grupos de tutorías actuales',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          rowsPerPage: 6,
+                          columns: <DataColumn>[
+                            DataColumn(label: Text('NOMBRE GRUPO')),
+                            DataColumn(label: Text('VER MÁS')),
+                            DataColumn(label: Text('ELIMINAR')),
+                          ],
+                          source: DynamicDataSource(
+                            _filteredData,
+                            _navigateToDetailsPage,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        ],
+      ),
     );
   }
+}
+
+class DynamicDataSource extends DataTableSource {
+  final List<Map<String, dynamic>> data;
+  final Function(String, String) onTap;
+  int _selectedRowCount = 0;
+
+  DynamicDataSource(this.data, this.onTap);
+
+  @override
+  DataRow? getRow(int index) {
+    if (index >= data.length) return null;
+    final rowData = data[index];
+    return DataRow(cells: <DataCell>[
+      DataCell(Text(rowData['NOMBRE'].toString())),
+      DataCell(
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () {
+            // onTap(rowData['CICLO'].toString(), rowData['DOCUMENTO'].toString());
+          },
+        ),
+      ),
+      DataCell(
+        IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () {
+            // onTap(rowData['CICLO'].toString(), rowData['DOCUMENTO'].toString());
+          },
+        ),
+      ),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => data.length;
+
+  @override
+  int get selectedRowCount => _selectedRowCount;
 }
